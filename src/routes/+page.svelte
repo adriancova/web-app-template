526783cc23a11f1f3ef72f419ed4bffb0e7c7589
adriancova <adriancova.95@gmail.com>
2024-04-08 10:42:25 -0600

    improved options and responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
 	import * as m from '$lib/paraglide/messages.js';
 
 	const options = [
@@ -12,14 +13,18 @@
 	];
 </script>
 
-<div class="container flex flex-col items-center justify-center space-y-5">
-	<h1 class="my-12 text-4xl font-extrabold tracking-tight lg:text-3xl">{m.welcome()}</h1>
+<div class="container flex flex-col items-center justify-center space-y-5 text-center">
+	<h1 class="my-8 text-4xl font-extrabold tracking-tight md:my-12 lg:text-3xl">
+		{m.welcome()}
+	</h1>
 
 	<h2 class="text-xl font-bold">{m.explore()}</h2>
-	<ul class="list-disc space-y-5 text-center">
+	<ul class="space-y-3 md:space-y-6">
 		{#each options as option}
 			<li>
-				<a href={option.route} target={option.external ? '_blank' : '_self'}>{option.label}</a>
+				<Button variant="outline" href={option.route} target={option.external ? '_blank' : '_self'}
+					>{option.label}</Button
+				>
 			</li>
 		{/each}
 	</ul>
