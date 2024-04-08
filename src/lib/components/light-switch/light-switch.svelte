e9b3fcbb039bce30f3ee0007ab89f4868b774d02
adriancova <adriancova.95@gmail.com>
2024-04-08 09:04:13 -0600

    added light-switch component

---
+++ src/lib/components/light-switch/light-switch.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import Icon from '$lib/components/ui/Icon.svelte';
+
+	import { toggleMode } from 'mode-watcher';
+</script>
+
+<Button on:click={toggleMode} variant="outline" size="icon">
+	<Icon
+		name="sun"
+		class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+	/>
+	<Icon
+		name="moon"
+		class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+	/>
+	<span class="sr-only">Toggle theme</span>
+</Button>
